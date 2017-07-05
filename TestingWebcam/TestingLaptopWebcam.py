99b9a94c692ba8547e3a195ae0307220484d63bb
Dean <deansmills@gmail.com>
2017-07-05 02:21:32 +0200

    Can get cideo from webcam, OpenCV bug prevents window from being properly closed

--- TestingWebcam/TestingLaptopWebcam.py
+++ TestingWebcam/TestingLaptopWebcam.py
@@ -5,3 +5,19 @@ Created on Tue Jul  4 13:11:57 2017
 @author: dean
 """
 
+import cv2
+import numpy as np
+
+VideoFeed = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = VideoFeed.read()
+    cv2.imshow('frame', frame)
+    
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+VideoFeed.release()
+cv2.destroyWindow(frame)
+cv2.waitKey(-1)
